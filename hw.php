26c889b5fc8f72ead75b55373f8a459be6367771
yparisien <yohann.parisien@aufeminin.com>
2016-12-31 12:20:25 +0100

    Intégration HTML
    Rajout d'un squellete HTML sur la page

--- hw.php
+++ hw.php
@@ -1,3 +1,33 @@
 <?php
-
-echo "Hello World";
+$titre = "Bonjour le monde";
+?>
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+		<meta charset="UTF-8">
+		<title>Hello World</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/knacss.css" media="all">
+		<link rel="stylesheet" href="css/styles.css" media="all">
+</head>
+<body>
+	
+	<header id="header" role="banner" class="line pam">
+		<h1><?php echo $titre; ?></h1>
+    <nav id="navigation" role="navigation">
+			<ul class="pam">
+				<li class="pam inbl">Bonne</li>
+				<li class="pam inbl">Année</li>
+				<li class="pam inbl">2017</li>
+			</ul>
+		</nav>
+	</header>
+	<div id="main" role="main" class="line pam">
+			<p class="">Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et une flammekueche. Ah so ? T'inquiète, ch'ai ramené du schpeck, du chambon, un kuglopf et du schnaps dans mon rucksack. Allez, s'guelt ! Wotch a kofee avec ton bibalaekaess et ta wurscht ? Yeuh non che suis au réchime, je ne mange plus que des Grumbeere light et che fais de la chym avec Chulien. Tiens, un rottznoz sur le comptoir.</p>
+			<p class="">Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
+	</div>
+	<footer id="footer" role="contentinfo" class="line pam txtcenter">
+		Lorem Elsass ipsum lacus leverwurscht Wurschtsalad mamsell Gal. gewurztraminer turpis, suspendisse commodo Oberschaeffolsheim ornare aliquam semper Miss Dahlias Mauris turpis sagittis kuglopf eleifend dignissim baeckeoffe geht's Richard Schirmeck mollis habitant schnaps ante et sit leo schpeck sit Salu bissame Salut bisamme varius quam. amet elementum nullam bissame bredele Heineken picon bière gal sed risus, condimentum Verdammi ch'ai ac réchime météor barapli s'guelt quam, non Christkindelsmärik blottkopf, Carola tellus rucksack vielmols, Gal !
+	</footer>
+</body>
+</html>
